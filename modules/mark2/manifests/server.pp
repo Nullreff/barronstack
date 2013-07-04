4cb75187a5c2720f7c1c1b31e84daf9255941709
Ryan Mendivil <rsmendivil@gmail.com>
2013-07-03 22:52:33 -0800

    Add mark2 class for setting up a server

---
+++ modules/mark2/manifests/server.pp
@@ -0,0 +1,71 @@
+# Class: mark2::server
+#
+# Sets up a minecraft server container running on mark2
+#
+# Parameters:
+#   [*server*] - Server name
+#   [*user*] - The user name to run the server under
+#   [*home*] -  The directory to run the server out of
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#   class { 'mark2::server':
+#     server => 'minecraft',
+#     user => 'minecraft',
+#     home => '/home/minecraft',
+#   }
+#
+class mark2::server(
+  $server = 'minecraft',
+  $user = $server,
+  $home = "/home/${user}",
+) {
+  $paths = ['/bin', '/sbin', '/usr/bin', '/usr/sbin']
+
+  include 'mark2'
+
+  case $::operatingsystem {
+    centos, redhat: {
+      $javaPackage = 'java-1.7.0-openjdk'
+    }
+    debian, ubuntu: {
+      $javaPackage = 'openjdk-7-jre-headless'
+    }
+  }
+
+  package { $javaPackage:
+    ensure => installed,
+  }
+
+  file { $home:
+    ensure => directory,
+    owner  => $user,
+    group  => $user,
+  }
+
+  file { "/etc/init.d/${server}":
+    ensure  => present,
+    content => template('mark2/service.sh.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  service { $server:
+    enable  => true,
+    require => File["/etc/init.d/${server}"],
+  }
+
+  user { $user:
+    ensure => present,
+    shell  => '/bin/false',
+    home   => $home,
+  }
+
+  group { $user:
+    ensure => present,
+  }
+}
